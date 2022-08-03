d88f4199e63eac904076c3e8a4cacba90e0d3be6
Jyothin Movva <jyothin.movva@gmail.com>
2022-08-03 12:36:02 +0530

    updated database setup

--- database_setup.sql
+++ database_setup.sql
@@ -1,3 +1,2 @@
-create database fake_new_detection; 
-create table userinfo(u_no int primary key, u_email text,u_input text); 
-create table input_data(u_no int primary key, u_input text); 
+create database fakenews; 
+create table user_info(email text,data text); 
